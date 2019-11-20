69b46c0e91675d9b1ee362902f1f7bdd8fd57fa1
Saloua Peatier <peatier@cerfacs.fr>
2019-11-20 17:42:38 +0100

    delete old/empty files

--- process_ensemble.py
+++
@@ -1,9 +0,0 @@
-%matplotlib inline
-import xarray as xr
-import glob
-import os
-
-path = "/scratch/globc/dcom/ARPEGE6_TUNE"
-
-da_model=xr.open_mfdataset(path+'/PRE623TUN010*.nc',concat_dim='ensemble')
-da_model_concat = xr.concat(da_model, dim='ensemble')
